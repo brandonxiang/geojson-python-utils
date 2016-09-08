f6b29c44e3a7baad98772448ee170deb799a9bfd
brandonxiang <1542453460@qq.com>
2016-09-08 11:25:06 +0800

    add function get_endpoint_from_points

--- geojson_utils/merger.py
+++ geojson_utils/merger.py
@@ -3,6 +3,14 @@ from copy import deepcopy
 from geojson_utils import point_distance
 
 def merge_featurecollection(*jsons):
+    """
+    merge features into one featurecollection
+
+    Keyword arguments:
+    jsons   -- jsons object list 
+
+    return geojson featurecollection
+    """
     features = []
     for json in jsons:
         if json['type'] == 'FeatureCollection':
@@ -12,7 +20,15 @@ def merge_featurecollection(*jsons):
 
 def simplify_other(major, minor, dist):
     """
-    point featurecollection only
+    Simplify the point featurecollection of poi with another point features accoording by distance.
+    Attention: point featurecollection only
+
+    Keyword arguments:
+    major    -- major geojson
+    minor    -- minor geojson
+    dist     -- distance 
+
+    return a geojson featurecollection with two parts of featurecollection
     """
     result = deepcopy(major)
     if major['type'] == 'FeatureCollection' and minor['type'] == 'FeatureCollection':
@@ -36,3 +52,35 @@ def simplify_other(major, minor, dist):
             if is_accept:
                 result["features"].append(minorfeature)
     return result
+
+def get_endpoint_from_points(points):
+    """
+
+    """
+    count = 0 
+    result = deepcopy(points)
+    if points['type'] == 'FeatureCollection':
+        feature_count = len(points['features'])
+        for i in range(0, feature_count):
+            first_geom = points['features'][i]['geometry']
+            first_lng = first_geom['coordinates'][0]
+            first_lat = first_geom['coordinates'][1]
+
+            for j in range(0, feature_count):
+                if i == j:
+                    continue
+                second_geom = points['features'][j]['geometry']
+                second_lng = second_geom['coordinates'][0]
+                second_lat = second_geom['coordinates'][1]
+
+                if first_lat == second_lat and first_lng == second_lng:
+                    result['features'].remove(points['features'][i])
+                    count += 1
+                    break
+    print(count)
+    return result
+
+
+def get_endpoint_from_linestring(linesting):
+    pass
+
